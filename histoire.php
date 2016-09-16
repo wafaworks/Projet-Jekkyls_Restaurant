fe3300f25d27cb03648bea415ef60b8391cb82f8
wafaworks <wafa.adj@gmail.com>
2016-09-16 03:15:49 +0200

    add histoire.css, changed histoire.php

--- histoire.php
+++ histoire.php
@@ -1,43 +1,31 @@
 <?php
-$pageTitle = "Notre Histoire";
+$pageTitle = "Notre Histoire : Les Fondateurs de Jekkyls' Restaurant et leur fameuse ratatouille";
 include 'include/header.php';
 ?>
-<div class="saut"</div>
+
 <article>
-	<div class="container-fluid"> 
-		<div class="row">
-			<div class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-1 col-lg-3 col-lg-offset-2" id="text1">
-				<h3>Les pères fondateurs</h3>
-				<div class="saut"></div>
-				<div class="saut"></div>
-				<h4>Damien Socket</h4>
-				<div class="saut"></div>
-				<div class="saut"></div>
-				<img src="img/fondateur1.jpg" title="Damien Socket" keyword="chef, cuisinier, damien, socket"/>
-				<p>Depuis sa tendre enfance Damien a noué un vrai lien affectif avec la cuisine conviviale et gourmande. Apprenti cuisinier chez Paul Bocuse à l'âge de 18 ans, il a su faire ses preuves en gastronomie. Il considère la cuisine comme un moyen de faire passer des émotions, de raconter une histoire tout en sublimant les petits rien de la vie. En vrai passionné, il a su grimper les échelons avec brio. Avec Jekkyls il a voulu revenir à ses premiers amours tout en essayant de briser les codes de la restauration haut de gamme. </p>
-				<div class="saut"></div>
-				<div class="saut"></div>
-				<div class="saut"></div>
-				<h4>Laurent Outan</h4>
-				<div class="saut"></div>
-				<div class="saut"></div>
-				<img src="img/fondateur2.jpg" title="Laurent Outan" keyword="chef, cuisinier, laurent, outan"/>
-				<div class="saut"></div>
-				<div class="saut"></div>
+	<div class="history" id="about-us"> 
+		<h1>Les pères fondateurs de JEKKYLS' Restaurant</h1>
+		<div class="fondators">
+			<div class="description" id="damien">
+				<img src="img/fondateur1.jpg" title="Damien Socket : chef cuisinier" alt="Damien Socket, co-fondateur Jekkyls" id="image-damien"/>
+				<h2>Damien Socket</h2>
+				<p>Depuis sa tendre enfance Damien a noué un vrai lien affectif avec la cuisine conviviale et gourmande. Apprenti cuisinier chez Paul Bocuse à l'âge de 18 ans, il a su faire ses preuves en gastronomie. Il considère la cuisine comme un moyen de faire passer des émotions, de raconter une histoire tout en sublimant les petits rien de la vie. En vrai passionné, il a su grimper les échelons avec brio. En co-fondant Jekkyls il a voulu revenir à ses premiers amours de cuisine tout en essayant de briser les codes de la restauration haut de gamme. </p>
+		    </div>
+		    <div class="description" id="laurent">
+				<img src="img/fondateur2.jpg" title="Laurent Outan: chef cuisinier" alt="Laurent Outan, co-fondateur Jekkyls" id="image-laurent"/>
+				<h2>Laurent Outan</h2>
 				<p>"Depuis sa tendre enfance Laurent a noué un vrai lien affectif avec la cuisine conviviale et gourmande. Apprenti cuisinier chez Philippe Etchebest à l'âge de 20 ans, il a su faire ses preuves en bistronomie pour commencer. Il considère la cuisine comme un moyen de faire passer de la joie, de raconter plein de choses sympa tout en sublimant les petits rien de la vie. En vrai passionné, il a su grimper les échelons avec brio. Avec Jekkyls il a voulu revenir à ses premiers amours tout en essayant de briser les codes de la restauration haut de gamme."</p>
 			</div>
-			<div class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-1 col-lg-3 col-lg-offset-2" id="text2">
-				<h2>Les origines de la ratatouille</h2>
-				<div class="saut"></div>
-				<div class="saut"></div>
-				<img src="img/ratatouille.jpg" title="Ratatouille" keyword="ratatouille, legumes, recette"/>
-				<div class="saut"></div>
-				<div class="saut"></div>
-				<div class="saut"></div>
-				<p>Elle est composée de légumes cuits (coupés en rondelles ou en quartiers), aubergines, courgettes, poivrons et tomates, ainsi que des oignons, de l'ail et de l'huile d'olive. Il n'y a pas de recette précise mais des principes à respecter. Deux méthodes sont possibles : cuisson de tous les légumes ensemble ou première cuisson légume par légume. Les défenseurs de la tradition provençale comme les grands cuisiniers Vergé ou Gedda, préconisent de faire revenir ou même frire les légumes un par un et de passer le poivron sur la flamme pour le débarrasser de la peau et lui donner un goût de grillé.</p>
-				
-			</div>
 		</div>
+		<div class="history" id="rencontre">
+		 	<p>La rencontre des deux co-fondateurs de Jekkyls' Restaurant s'est faite il y a bien longtemps à Londres, alors qu'ils venaient de s'installer pour travailler dans un restaurant typique londonnien. Ils ont pu apprendre de la culture anglosaxonne mais surtout de leurs méthode de cuisine et de leurs plats typique. Pour eux alors, malgré leur jeune insoucience, il ne faisait aucun doute que la cuisine française dégageait plus d'émotions et de panache. Il s'était alors promis que si plus tard tout avait bien marché pour eux, ils montraient leur propre affaire qui ammènerait les bons plats de leur enfance à un niveau supérieur. Promesse tenue avec Jekkyls' Restaurant, en référence à Jekyll et Mr Hyde - personnage fictif londonnien - petit clin d'oeil sur leur rencontre donc ! </p>
+		</div>
+	</div>
+	<div class="history" id="recette">
+		<h2>Les origines de la ratatouille</h2>
+		<img src="img/ratatouille.jpg" title="Ratatouille: la spécialité de Jekkyls' Restaurant" alt="Une bonne ratatouille proposée chez Jekkyls" id="ratatouille"/>
+		<p>Elle est composée de légumes cuits (coupés en rondelles ou en quartiers), aubergines, courgettes, poivrons et tomates, ainsi que des oignons, de l'ail et de l'huile d'olive. Il n'y a pas de recette précise mais des principes à respecter. Deux méthodes sont possibles : cuisson de tous les légumes ensemble ou première cuisson légume par légume. Les défenseurs de la tradition provençale comme les grands cuisiniers Vergé ou Gedda, préconisent de faire revenir ou même frire les légumes un par un et de passer le poivron sur la flamme pour le débarrasser de la peau et lui donner un goût de grillé.</p>
 	</div>
 </article>
 
