e93a729b12d662b7adfa4dad3cf03c1b7c495c95
Alexandre FRADET <a_fradet@yahoo.fr>
2016-09-16 11:30:38 +0200

    Fix some css

--- histoire.php
+++ histoire.php
@@ -10,15 +10,16 @@ include 'include/header.php';
 			<div class="description" id="damien">
 				<img src="img/fondateur1.jpg" title="Damien Socket : chef cuisinier" alt="Damien Socket, co-fondateur Jekkyls" id="image-damien"/>
 				<h2><strong>Damien Socket</strong></h2>
+						<div id="separ"></div>
 				<p>Depuis sa tendre enfance Damien a noué un vrai lien affectif avec la cuisine conviviale et gourmande. Apprenti cuisinier chez Paul Bocuse à l'âge de 18 ans, il a su faire ses preuves en gastronomie. Il considère la cuisine comme un moyen de faire passer des émotions, de raconter une histoire tout en sublimant les petits rien de la vie. En vrai passionné, il a su grimper les échelons avec brio. En co-fondant Jekkyls il a voulu revenir à ses premiers amours de cuisine tout en essayant de briser les codes de la restauration haut de gamme. </p>
 		    </div>
-		    <div class="spacing">
-		    	<p><br/><p>
+		    <div id="separ">
 
 		    </div> <!-- Ne surtout pas faire normalement! mais là je ne trouve pas de solution dans le temps qu'il me reste.. -->
 		    <div class="description" id="laurent">
 				<img src="img/fondateur2.jpg" title="Laurent Outan: chef cuisinier" alt="Laurent Outan, co-fondateur Jekkyls" id="image-laurent"/>
 				<h2><strong>Laurent Outan</strong></h2>
+						<div id="separ"></div>
 				<p>Depuis sa tendre enfance Laurent a noué un vrai lien affectif avec la cuisine conviviale et gourmande. Apprenti cuisinier chez Philippe Etchebest à l'âge de 20 ans, il a su faire ses preuves en bistronomie pour commencer. Il considère la cuisine comme un moyen de faire passer de la joie, de raconter plein de choses sympa tout en sublimant les petits rien de la vie. En vrai passionné, il a su grimper les échelons avec brio. Avec Jekkyls il a voulu revenir à ses premiers amours tout en essayant de briser les codes de la restauration haut de gamme.</p>
 			</div>
 		</div>
@@ -27,15 +28,17 @@ include 'include/header.php';
 		</div>
 	</div>
 	<div class="history" >
-		<h2>Les origines de la ratatouille</h2>
+		<div id="separ"></div>
+		<h2><strong>Les origines de la ratatouille</strong></h2>
+				<div id="separ"></div>
 		<img src="img/ratatouille.jpg" title="Ratatouille: la spécialité de Jekkyls' Restaurant" alt="Une bonne ratatouille proposée chez Jekkyls" id="ratatouille"/>
-		<div class="spacing">
-	    	<p><br/><p>
+		<div id="separ">
 		</div> <!-- Ne surtout pas faire normalement! mais là je ne trouve pas de solution dans le temps qu'il me reste.. -->
 		<div class= recette>
 		<p>Elle est composée de légumes cuits (coupés en rondelles ou en quartiers), aubergines, courgettes, poivrons et tomates, ainsi que des oignons, de l'ail et de l'huile d'olive. Il n'y a pas de recette précise mais des principes à respecter. Deux méthodes sont possibles : cuisson de tous les légumes ensemble ou première cuisson légume par légume. Les défenseurs de la tradition provençale comme les grands cuisiniers Vergé ou Gedda, préconisent de faire revenir ou même frire les légumes un par un et de passer le poivron sur la flamme pour le débarrasser de la peau et lui donner un goût de grillé.</p>
 		</div>
 	</div>
+					<div id="separ"></div>
 </article>
 
 <?php
