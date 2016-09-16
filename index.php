c858fae2ea287d191d4b368993a485eb9ebbac5f
Alexandre FRADET <a_fradet@yahoo.fr>
2016-09-16 04:52:15 +0200

    Add carousel and timer for it

--- index.php
+++ index.php
@@ -15,23 +15,61 @@ include 'include/header.php';
 <article>
 	<div id="accanchor">
 	</div>
-	<div class="container"> 
+	<div class="container-fluid"> 
 		<div class="row">
-			<div class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-1" id="text1">
+			<div class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-1 col-lg-3 col-lg-offset-2" id="text1">
 				<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
 				</p>
 				<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
 				</p>
 			</div>
-			<div class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-2" id="text2">
-				<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-				</p>
-				<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-				</p>
-				<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-				</p>
-				<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-				</p>
+			<div class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-2 col-lg-3 col-lg-offset-2" id="text2">
+				<div id="carouActu" class="carousel slide" data-ride="carousel">
+  					<!-- Indicators -->
+  					<ol class="carousel-indicators">
+ 					   <li data-target="#carouActu" data-slide-to="0" class="active"></li>
+ 					   <li data-target="#carouActu" data-slide-to="1"></li>
+ 					   <li data-target="#carouActu" data-slide-to="2"></li>
+   					</ol>
+
+ 					 <!-- Wrapper for slides -->
+ 					<div class="carousel-inner" role="listbox">
+   						<div class="item active">
+  						  <img src="img/act_gordon.png" alt="Gordon">
+                <div class="carousel-caption">
+                  <h3>Attention il arrive !</h3>
+                  <p>Gordon Ramsay vient nous livrer sa version de la Ratatouille.</p>
+                </div>
+    					</div>
+
+    					<div class="item">
+    						<img src="img/act_london.jpg" alt="London">
+                <div class="carousel-caption">
+                  <h3>Jekylls' bientôt à Londres !</h3>
+                  <p>Très prochainement l'ouverture de notre première franchise à l'étranger !</p>
+                </div>
+ 						</div>
+
+ 					    <div class="item">
+     						<img src="img/act_mich.jpg" alt="Michelin">
+                <div class="carousel-caption">
+                  <h3>Jekylls' reconnu pour sa qualité</h3>
+                  <p>Tous nos restaurants présents dans l'édition 2016 du guide Michelin !</p>
+                </div>
+    					</div>
+
+  				</div>
+
+  					<!-- Left and right controls -->
+  					<a class="left carousel-control" href="#carouActu" role="button" data-slide="prev">
+    					<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    					<span class="sr-only">Previous</span>
+  					</a>
+  					<a class="right carousel-control" href="#carouActu" role="button" data-slide="next">
+   						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    					<span class="sr-only">Next</span>
+  					</a>
+				</div>
 			</div>
 		</div>
 	</div>
--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+// Ouverture fiche restaurants quand link par footer
+
 $(document).ready(function () {
   location.hash && $(location.hash + '.collapse').collapse('show');
+  setInterval(function(){chgmt(1)},3000);
 });
+
+// Changement automatique du carousel toutes les 2s.
